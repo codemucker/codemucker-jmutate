be2b4dcb8190f72e3bcfc600d738b644d9883773
Bert van Brakel <bertvanbrakel@gmail.com>
2014-12-09 11:08:12 +1030

    If replacing methods, ensure same location as existing is used

--- src/main/java/org/codemucker/jmutate/transform/InsertMethodTransform.java
+++ src/main/java/org/codemucker/jmutate/transform/InsertMethodTransform.java
@@ -2,12 +2,15 @@ package org.codemucker.jmutate.transform;
 
 import static com.google.common.base.Preconditions.checkState;
 
+import java.util.List;
+
 import org.codemucker.jfind.FindResult;
 import org.codemucker.jmutate.JMutateException;
 import org.codemucker.jmutate.PlacementStrategy;
 import org.codemucker.jmutate.ast.ContextNames;
 import org.codemucker.jmutate.ast.JMethod;
 import org.codemucker.jmutate.ast.matcher.AJMethod;
+import org.eclipse.jdt.core.dom.ASTNode;
 import org.eclipse.jdt.core.dom.MethodDeclaration;
 
 import com.google.inject.Inject;
@@ -26,33 +29,37 @@ public final class InsertMethodTransform extends AbstractNodeInsertTransform<Ins
 		checkFieldsSet();
 		checkState(method != null, "missing method");
 		
-	    //TODO:detect if it exists?
-		boolean insert = true;
-		FindResult<JMethod> found = getTarget().findMethodsMatching(AJMethod.with().nameAndArgSignature(method));
+	    FindResult<JMethod> found = getTarget().findMethodsMatching(AJMethod.with().nameAndArgSignature(method));
 		if(!found.isEmpty()){
-			insert = false;
 			JMethod existingMethod = found.getFirst();
 			switch(getClashStrategy()){
 			case REPLACE:
-			    //todo:remember where this method is
+			    //put in same location as existing
+				insert(method.getAstNode(), existingMethod.getAstNode());
 				existingMethod.getAstNode().delete();
-				insert = true;
-				break;
-			case IGNORE:
-				break;
+				return;
+			case SKIP:
+				return;
 			case ERROR:
 				throw new JMutateException("Existing method:\n %s\n, not replacing with %s", existingMethod.getAstNode(), method);
 			default:
 				throw new JMutateException("Existing method:\n %s\n, unsupported clash strategy %s", existingMethod.getAstNode(), getClashStrategy());
 			}
 		}
-		if(insert){
-			new NodeInserter()
-				.nodeToInsert(method.getAstNode())
-				.target(getTarget())
-				.placementStrategy(getPlacementStrategy())
-				.insert();
-		}
+		
+		insert(method.getAstNode(), null);
+	}
+
+	private void insert(MethodDeclaration m, ASTNode beforeNode){
+
+		PlacementStrategy placement = new InsertSameLocationPlacementStrategy(getPlacementStrategy(),beforeNode);
+		
+		new NodeInserter()
+			.nodeToInsert(method.getAstNode())
+			.target(getTarget())
+			.placementStrategy(placement)
+			.insert();
+		
 	}
 
 	/**
@@ -82,5 +89,31 @@ public final class InsertMethodTransform extends AbstractNodeInsertTransform<Ins
 	public InsertMethodTransform method(JMethod newMethod) {
     	this.method = newMethod;
     	return this;
+	}
+	
+	private static class InsertSameLocationPlacementStrategy implements PlacementStrategy {
+
+		private final PlacementStrategy fallbackStrategy;
+		private final ASTNode afterNode;
+		
+		public InsertSameLocationPlacementStrategy(PlacementStrategy fallbackStrategy,
+				ASTNode afterNode) {
+			super();
+			this.fallbackStrategy = fallbackStrategy;
+			this.afterNode = afterNode;
+		}
+
+		@Override
+		public int findIndexToPlaceInto(ASTNode nodeToInsert,
+				List<ASTNode> nodes) {
+			if(afterNode != null){
+				for(int i = 0; i < nodes.size();i++){
+					if( nodes.get(i)== afterNode){
+						return i+1;
+					}
+				}
+			}
+			return fallbackStrategy.findIndexToPlaceInto(nodeToInsert, nodes);
+		}
 	}
 }
